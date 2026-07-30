f247bae8ce3daf40df9d1cac4498752abd47d81a
Leo-emp <pmzo.mm08@gmail.com>
2026-07-31 00:29:11 +0700

    feat(db): add repository barrel export

---
+++ src/db/repositories/index.ts
@@ -0,0 +1,7 @@
+// Barrel for all repository modules.
+// Usage: import { createLearner, findActiveSession, ... } from '@/db/repositories'
+export * from './learners'
+export * from './test-sessions'
+export * from './test-results'
+export * from './diagnoses'
+export * from './placement'
